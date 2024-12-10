2f04fa0631fd27e6a309c1784c2fca68f60ff88e
miyatakazuki6789 <miyatakazuki6789@github.com>
2024-12-10 19:04:56 +0700

    init

---
+++ l-e-z-h-i-n.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         l-e-z-h-i-n
+// @namespace    l-e-z-h-i-n.user.js
+// @version      2024.12.10
+// @description  down l-e-z-h-i-n
+// @author       You
+// @include      /^https:\/\/www\.l[e4]zh[i1]n\.c[o0]m\/k[o0]\/c[o0]mic\/.+?\/.+/
+// @include      /^https:\/\/www\.l[e4]zh[i1]nus\.c[o0]m\/en\/c[o0]mic\/.+?\/.+/
+// @include      /^https:\/\/www\.l[e4]zh[i1]n\.jp\/ja\/c[o0]mic\/.+?\/.+/
+// @icon         https://raw.githubusercontent.com/miyatakazuki6789/bong_nga_v4_tay/main/l-e-z-h-i-n.png
+// @require      https://unpkg.com/file-saver@2.0.5/dist/FileSaver.min.js
+// @grant        none
+// @downloadURL  https://github.com/miyatakazuki6789/bong_nga_v4_tay/raw/main/l-e-z-h-i-n.user.js
+// @updateURL    https://github.com/miyatakazuki6789/bong_nga_v4_tay/raw/main/l-e-z-h-i-n.meta.js
+// ==/UserScript==
